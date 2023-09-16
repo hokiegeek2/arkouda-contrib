01310e9dea292425e9027d9e59dc371acd52ef86
hokiegeek2 <hokiegeek2@users.noreply.github.com>
2023-09-16 09:25:57 -0400

    updated per model change #104

--- arkouda_proxy_client/arkouda_proxy_client/arkouda_pb2.pyi
+++ arkouda_proxy_client/arkouda_proxy_client/arkouda_pb2.pyi
@@ -23,9 +23,17 @@ class ArkoudaRequest(_message.Message):
     def __init__(self, user: _Optional[str] = ..., token: _Optional[str] = ..., cmd: _Optional[str] = ..., format: _Optional[str] = ..., size: _Optional[int] = ..., args: _Optional[str] = ..., request_id: _Optional[str] = ...) -> None: ...
 
 class ArkoudaResponse(_message.Message):
-    __slots__ = ["message", "request_id"]
+    __slots__ = ["args", "cmd", "message", "request_id", "request_status", "user"]
+    ARGS_FIELD_NUMBER: _ClassVar[int]
+    CMD_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
     REQUEST_ID_FIELD_NUMBER: _ClassVar[int]
+    REQUEST_STATUS_FIELD_NUMBER: _ClassVar[int]
+    USER_FIELD_NUMBER: _ClassVar[int]
+    args: str
+    cmd: str
     message: str
     request_id: str
-    def __init__(self, message: _Optional[str] = ..., request_id: _Optional[str] = ...) -> None: ...
+    request_status: str
+    user: str
+    def __init__(self, message: _Optional[str] = ..., request_id: _Optional[str] = ..., request_status: _Optional[str] = ..., user: _Optional[str] = ..., cmd: _Optional[str] = ..., args: _Optional[str] = ...) -> None: ...
